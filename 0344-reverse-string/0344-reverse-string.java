009f58e39e9fae8f475ee920c69326fe8422ae72
vinaypandey900 <vinaypandey900@yahoo.com>
2024-06-02 18:49:05 +0530

    Time: 0 ms (100%), Space: 45.2 MB (96.05%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -3,11 +3,19 @@ class Solution {
         int left=0;
         int right=s.length-1;
         while(left<right){
-            char temp=s[left];
-            s[left]=s[right];
-            s[right]=temp;
+            s[left]=(char)(s[left]^s[right]);
+            s[right]=(char)(s[left]^s[right]);
+            s[left]=(char)(s[left]^s[right]);
             left++;
             right--;
         }
+        
+        // while(left<right){
+        //     char temp=s[left];
+        //     s[left]=s[right];
+        //     s[right]=temp;
+        //     left++;
+        //     right--;
+        // }
     }
 }
